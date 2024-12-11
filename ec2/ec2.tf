7bae5e66921e9074aa96d2ee8d5a106b013a275e
Your Name <you@example.com>
2024-12-11 16:58:35 +0530

    msg

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -26,16 +26,15 @@ resource "aws_security_group" "allow_ssh_terraform" {
 
   # terraform will give us a variable with block name
 
-   dynamic "ingress" {
-    for_each = var.ingress_rules
-    content {
-    from_port        = ingress.value["from_port"] 
-    to_port          = ingress.value["to_port"]
-    protocol         = ingress.value["protocol"]
-    cidr_blocks      = ingress.value["cidr_blocks"] # Allow from everywhere
-    }
-  }
-tags = {
+ ingress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+ }
+ 
+ tags = {
      Name = "allow_sshh"
   }
 }
